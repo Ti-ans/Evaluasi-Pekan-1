42fb4c8d03c53b8db0d496bb01b25cab458d00d7
Raafi Septiansyah <raafiseptiansyah29@gmail.com>
2024-10-05 12:24:56 +0700

    Evaluasi

---
+++ Evaluasi.js
@@ -0,0 +1,101 @@
+// Tipe data 
+
+// 1. Number = Data yang berisikan number/ angka.
+// Contoh :
+var nilai = 15
+console.log(nilai)
+
+// 2. String = Data yang berisikan kata.
+// Contoh :
+var kata = "Selamat Pagi"
+document.write(kata)
+
+// 3. Boolean = Data yang berisikan 2 pilihan "true atau false".
+// Contoh : 
+let BB = 75
+let TB = 175
+console.log(BB >= 80 && TB >= 170 ? true : false)
+
+// Variabel 
+
+// Variabel = Wadah untuk menyimpan nilai, nilai ini berupa tipe data.
+
+// 1. Var = Variabel yang mendeklarasikan data secara cakupan global atau fungsi/lokal.
+// Contoh :
+var word = "Hai"
+word = "Salam kenal"
+console.log(word)
+
+// 2. Let = Variabel yang mendeklarasikan suatu data yang hanya bisa diakses di dalam block kode.
+// Contoh : 
+let namaDepan = "Upin"
+let namaBelakang = "Ipin"
+console.log(namaDepan + namaBelakang)
+
+// 3. Const = Variabel yang mendeklarasikan suatu data secara konstan.
+// Contoh : 
+const user = {
+    nama : 'Tians',
+    umur : 19
+}
+console.log(user)
+
+// Operator aritmetika 
+var nilai1 = 30
+var nilai2 = 5
+var Tambah = nilai1 + nilai2
+var Kurang = nilai1 - nilai2
+var Kali = nilai1 * nilai2
+var Bagi = nilai1 / nilai2
+console.log(Tambah)
+console.log(Kurang)
+console.log(Kali)
+console.log(Bagi)
+
+// Operator Penugasan 
+
+// Operator penugasan = Sistem operasi untuk membandingkan 2 buah nilai.
+// Contoh : 
+let angka = 50
+ angka += 5
+ angka -= 15
+ angka /= 2
+ angka *= 20
+console.log(angka)
+
+// Operator logika 
+
+// Operator logika = Operasi yang berisikan 2 pilihan : true atau false.
+
+// 1. Or
+var netto = 100
+if ((netto - 50 < 100) || (netto < 150)) {  
+    console.log("Hasilnya true");  
+} else {  
+    console.log("Hasilnya salah");  
+}
+// Hasil yang akan di tampilkan adalah true, karena hasilnya netto - 50 = < 100 atau netto < 150. Yang dimana kedua pilihan tersebut true.
+
+// 2. And
+let umur = 20
+let statusPekerjaan = "bekerja"
+if (umur >= 18 && statusPekerjaan === "bekerja") {  
+    console.log("Anda adalah seorang dewasa yang bekerja.");  
+} else {  
+    console.log("Anda tidak memenuhi syarat.");  
+}
+// Hasil yang akan ditampilkan Anda adalah seorang dewasa yang bekerja, karena apabila umur >= 18 dan status pekerjaannya bekerja, maka hasilnya Anda adalah seorang dewasa yang bekerja.
+
+// 3. Not
+console.log(!false)
+// Hasil yang akan ditampilkan adalah true, karena di dalam logika not antonim terhadap nilai sebenarnya.
+
+var mobil1 = putih 
+var asalBuatan = "jepang"
+if (mobil2 == jepang || mobil1 == hitam){
+    console.log("Mobil buatan Jepang")
+}else 
+    
+
+
+
